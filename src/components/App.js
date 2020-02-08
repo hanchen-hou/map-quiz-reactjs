eb9421f925e86c7763bfab515523696a20473d46
Hanchen <hanchen.hou@hotmail.com>
2020-02-07 21:56:57 -0800

    Initial commit from Create React App

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+
+    render() {
+        const className = "app";
+        return (
+            <div className={className}>
+            </div>
+        )
+    }
+}
+
+
